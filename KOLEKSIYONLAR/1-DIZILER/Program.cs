09488fa02d394d940e1a8e22f606cd0a5b05e300
ARDATR103 <celalbabacan1@gmail.com>
2021-12-31 11:44:32 +0300

    CREATED 31.12.2021 11:44

---
+++ KOLEKSIYONLAR/1-DIZILER/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_DIZILER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayılar = new int[5] { 30, 50, 75, 45, 90 };
+            string[] isimler = new string[5] { "UTKU", "EMO", "FİKO", "CELO", "TUTKU" };
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("SAYILAR : " + sayılar[i]);
+                Console.WriteLine("İSİMLER : " + isimler[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ KOLEKSIYONLAR/2-DIZI ORNEKLERI/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_DIZI_ORNEKLERI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ÖRNEK1
+
+            //random oluşturulan 20 sayılık dizinin tek sayılarını yazdırın
+            Random rnd = new Random();
+            int rast, sayac = 0;
+            int[] sayılar = new int[20];
+
+            for (int i = 0; i < 20; i++)
+            {
+                rast = rnd.Next(1, 100);
+                Console.WriteLine("RASTGELE SAYILAR :" + rast);
+                sayılar[i] = rast;
+            }
+
+            Console.WriteLine("---------------------------");
+
+            foreach (var item in sayılar)
+            {
+                if (item % 2 == 1)
+                {
+                    sayac++;
+                    Console.WriteLine("TEK SAYILAR : " + item);
+                }
+            }
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("TEK SAYI ADETİ : " + sayac);
+            Console.ReadKey();
+
+            #endregion
+        }
+    }
+}
