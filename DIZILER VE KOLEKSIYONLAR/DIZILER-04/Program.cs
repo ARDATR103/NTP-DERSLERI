ff261a68a711e931316e9363eaea116a15a74e3e
ARDATR103 <celalbabacan1@gmail.com>
2022-01-17 12:04:34 +0300

    .

--- DIZILER VE KOLEKSIYONLAR/DIZILER-04/Program.cs
+++ DIZILER VE KOLEKSIYONLAR/DIZILER-04/Program.cs
@@ -17,16 +17,47 @@ namespace DIZILER_04
             dinamikList.Add("arda");
             dinamikList.Add("celal");
 
-            foreach (var item in dinamikList)
+            foreach (object item in dinamikList)
             {
                 Console.WriteLine(item);
 
             }
             Console.WriteLine("----------");
             int sonuc;
-            sonuc = (int)dinamikList[0] * 2;
+            sonuc = (int)dinamikList[0] * 2; //(int) diyerek unbox ediyoruz işlem yapmak için gerekli
             Console.WriteLine("Sonuç : "+sonuc);
+            
+
+            Console.WriteLine("----------");
+            dinamikList[2] = "UTKU"; //var olan elemanı değiştirme
+            foreach (object item in dinamikList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("-----------");
+            dinamikList.Insert(3, "emirhan"); // 3 numaralı indise eleman ekler yani araya 3 numaralı elemanı silmeden eleman ekler
+            foreach (object item in dinamikList)
+            {
+                Console.WriteLine(item); 
+            }
+
+            Console.WriteLine("----------");
+            dinamikList.Remove("UTKU"); // girilen değerdeki  elemanı siler, diğer elemanlar aynı kalır
+            foreach (object item in dinamikList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("----------");
+            dinamikList.RemoveAt(3); // girilen indisteki elemanı siler,diğer elemanlar aynı kalır
+            foreach (object item in dinamikList)
+            {
+                Console.WriteLine(item);
+            }
+
             Console.ReadKey();
+            
         }
     }
 
