8fdc8e60d845faae453f7c0cfcfbd0915381580b
ARDATR103 <celalbabacan1@gmail.com>
2022-01-18 09:25:12 +0300

    .

---
+++ DIZILER VE KOLEKSIYONLAR/DIZILER-05/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace DIZILER_05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtŞehirler = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnArayaEkle = new System.Windows.Forms.Button();
+            this.btnGüncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.lstŞehirler = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Şehirler :";
+            // 
+            // txtŞehirler
+            // 
+            this.txtŞehirler.Location = new System.Drawing.Point(76, 18);
+            this.txtŞehirler.Name = "txtŞehirler";
+            this.txtŞehirler.Size = new System.Drawing.Size(100, 20);
+            this.txtŞehirler.TabIndex = 1;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(12, 72);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnArayaEkle
+            // 
+            this.btnArayaEkle.Location = new System.Drawing.Point(93, 72);
+            this.btnArayaEkle.Name = "btnArayaEkle";
+            this.btnArayaEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnArayaEkle.TabIndex = 2;
+            this.btnArayaEkle.Text = "Araya Ekle";
+            this.btnArayaEkle.UseVisualStyleBackColor = true;
+            this.btnArayaEkle.Click += new System.EventHandler(this.btnArayaEkle_Click);
+            // 
+            // btnGüncelle
+            // 
+            this.btnGüncelle.Location = new System.Drawing.Point(174, 72);
+            this.btnGüncelle.Name = "btnGüncelle";
+            this.btnGüncelle.Size = new System.Drawing.Size(75, 23);
+            this.btnGüncelle.TabIndex = 2;
+            this.btnGüncelle.Text = "Güncelle";
+            this.btnGüncelle.UseVisualStyleBackColor = true;
+            this.btnGüncelle.Click += new System.EventHandler(this.btnGüncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(255, 72);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 2;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(336, 72);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(75, 23);
+            this.btnAra.TabIndex = 2;
+            this.btnAra.Text = "Ara";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Location = new System.Drawing.Point(9, 159);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(38, 13);
+            this.lblDurum.TabIndex = 3;
+            this.lblDurum.Text = "Durum";
+            // 
+            // lstŞehirler
+            // 
+            this.lstŞehirler.FormattingEnabled = true;
+            this.lstŞehirler.Location = new System.Drawing.Point(291, 103);
+            this.lstŞehirler.Name = "lstŞehirler";
+            this.lstŞehirler.Size = new System.Drawing.Size(120, 147);
+            this.lstŞehirler.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 262);
+            this.Controls.Add(this.lstŞehirler);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGüncelle);
+            this.Controls.Add(this.btnArayaEkle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtŞehirler);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtŞehirler;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnArayaEkle;
+        private System.Windows.Forms.Button btnGüncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.Label lblDurum;
+        private System.Windows.Forms.ListBox lstŞehirler;
+    }
+}
+
---
+++ DIZILER VE KOLEKSIYONLAR/DIZILER-05/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace DIZILER_05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ArrayList Şehirler = new ArrayList();
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            Şehirler.Add(txtŞehirler.Text);
+            iller();
+        }
+
+        private void iller()
+        {
+            lstŞehirler.Items.Clear();
+            txtŞehirler.Text = null;
+            foreach (object şehir in Şehirler)
+            {
+                lstŞehirler.Items.Add(şehir);
+            }
+        }
+
+        private void btnArayaEkle_Click(object sender, EventArgs e)
+        {
+            int indexno = lstŞehirler.SelectedIndex;
+            Şehirler.Insert(indexno, txtŞehirler.Text);
+            iller();
+        }
+
+        private void btnGüncelle_Click(object sender, EventArgs e)
+        {
+            int indexno = lstŞehirler.SelectedIndex;
+            Şehirler[indexno] = txtŞehirler.Text;
+            iller();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            int indexno = lstŞehirler.SelectedIndex;
+            Şehirler.RemoveAt(indexno);
+            iller();
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            if (Şehirler.Contains(txtŞehirler.Text))
+            {
+                lblDurum.Text = "ARADIĞINIZI TER AKITIP BULDUK EFENDİM";
+                lstŞehirler.SelectedIndex = Şehirler.IndexOf(txtŞehirler.Text);
+            }
+
+            else
+            {
+                lblDurum.Text = "BULAMADIK EFENDİM";
+            }
+        }
+    }       
+}
